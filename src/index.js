d3a2872955e67e80a1ff43e4e54afa39ff91bcce
biopravda <30368360+biopravda@users.noreply.github.com>
2018-03-14 13:54:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+const createEnumerableProperty = (enumProperty) => {return enumProperty};
+const createNotEnumerableProperty = (enumProperty) => {
+    Object.defineProperty(Object.prototype, enumProperty, {
+        enumerable: false,
+        value: 'value'});
+        return enumProperty;};
+
+const createProtoMagicObject = () => {
+    function magicObj(){};
+
+    magicObj.prototype = magicObj.__proto__;
+
+    return magicObj;
+};
+let i = 0;
+const incrementor = () => {i++; return incrementor};incrementor.valueOf = () => i;
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const createSerializedObject = () => {return null};
 const toBuffer = () => {};
 const sortByProto = () => {};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
 exports.createProtoMagicObject = createProtoMagicObject;
+
+
 exports.incrementor = incrementor;
 exports.asyncIncrementor = asyncIncrementor;
 exports.createIncrementer = createIncrementer;
